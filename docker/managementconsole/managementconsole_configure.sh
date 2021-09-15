01185761b9104dcf96063532b1f1dfcae7a5a166
KofaxRPA <47416964+KofaxRPA@users.noreply.github.com>
2021-09-15 16:33:44 +0200

    Update managementconsole_configure.sh

--- docker/managementconsole/managementconsole_configure.sh
+++ docker/managementconsole/managementconsole_configure.sh
@@ -13,14 +13,17 @@ MC_REST()
 }
 MC_Change_Password()
 {
-    user=$1; oldpassword=$2; newpassword=$3
-    data="--data {\"newPassword\":\"${newpassword}\",\"sendEmail\":false,\"username\":\"${username}\",\"oldPassword\":\"${oldpassword}\"}"
+    username=$1; oldPassword=$2; newPassword=$3
+    data="--data {\"newPassword\":\"${newPassword}\",\"sendEmail\":false,\"username\":\"${username}\",\"oldPassword\":\"${oldPassword}\"}"
     MC_REST PUT "/api/mc/user/resetPassword" "-H \"Content-Type: text/plain\"" ${data} 
 }
 MC_Create_User()
 {
-    userName=$1; password=$2; fullname=$3; email=$4, GroupNames=$5
-    data="--data {\"emailAddress\":\"${email}\",\"fullName\":\"${fullName}\",\"password\":\"${password}\",\"userName\":\"${userName}}\",\"groupNames\":${GroupNames}}"
+    userName=$1; password=$2; fullname=$3; email=$4, groupNames=$5
+    #jsonify group names
+    groupNames=${groupNames/,/],[/}
+    data="--data {\"emailAddress\":\"${email}\",\"fullName\":\"${fullName}\",\"password\":\"${password}\",\"userName\":\"${userName}}\",\"groupNames\":${[groupNames]}}"
+    echo ${data}
     MC_REST POST "/api/mc/user/add" "Content-Type: text/plain" ${data} 
 }
 
@@ -40,15 +43,17 @@ MC_Add_Robot()
     MC_REST POST "/api/mc/robot/add" -F fileField=@${FILEPATH}${FILENAME} - F projectId="13" -F commitMessage="" -F folderName="${folderName}" -F override="false"
 }
 
-MC_Change_Password admin admin 2308ewtro32
-MC_Add_Group developers
-MC_Add_Group roboservers
-MC_Add_Group synchronizers
-MC_Add_Group kappletadmins
-MC_Add_Group kappletusers
-MC_Add_User david £$%(*)$£% "David Wright" david.wright@kofax.com ["Developers"],["admin"],["kappletadmins"],["kappletusers"]
-MC_Add_User roboserver "£$"£$ Roboserver roboserver@kofax.com ["Roboservers"]
-MC_Add_User synch £$%)£$% Synch synch@kofax.com ["Synchronizers"]
+MC_Change_Password ${USERNAME} admin ${PASSWORD}
+MC_Add_Group Developers
+MC_Add_Group Roboservers
+MC_Add_Group Synchronizers
+MC_Add_Group KappletAdmins
+MC_Add_Group KappletUsers
+MC_Add_User david £$%(*)$£% "David Wright" david.wright@kofax.com "Developers,RPA Administrators,KappletAdmins,KappletUsers"
+MC_Add_User roboserver 4£^6£y Roboserver roboserver@kofax.com Roboservers
+MC_Add_User synch £tU%_£3% Synch synch@kofax.com "Synchronizers"
+
+#13 is the id of the "Default Project"
 MC_Add_Robot 13 "/samplerobots/" "abc.robot" ""
 MC_Add_Robot 13 "/samplerobots/" "abc.robot" ""
 
