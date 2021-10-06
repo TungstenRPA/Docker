0baa792ab6339ded2188ab10ef83b288fcdb4b1a
David Wright <david.wright@kofax.com>
2021-10-06 20:13:33 +0200

    put back missing roboserver config

--- docker/roboserver/roboserver.sh
+++ docker/roboserver/roboserver.sh
@@ -15,5 +15,11 @@ do
 done
 echo Roboserver can log into Management Console
 
+if [ ! -f "${KAPOW_HOME}/data/Configuration/roboserver.settings" ]; then
+   # run ConfigureMC to get (most of) the data-folder created
+   ${KAPOW_HOME}/bin/ConfigureRS 1> /dev/null 2>&1
+   # then we can (re-)configure
+   ${KAPOW_HOME}/jre/bin/java -jar /roboServerConfigurator.jar
+fi
 
 ${KAPOW_HOME}/bin/RoboServer $@
