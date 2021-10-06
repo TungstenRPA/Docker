aaf5f6becb3abd96ff2e6295511290182f32eb9f
David Wright <david.wright@kofax.com>
2021-10-06 15:08:05 +0200

    fixed API call

--- docker/roboserver/roboserver.sh
+++ docker/roboserver/roboserver.sh
@@ -6,7 +6,7 @@
 while :
 do
     echo check if Management Console has created Roboserver account
-    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -u ${ROBOSERVER_MC_USERNAME}:${ROBOSERVER_MC_PASSWORD} http://localhost:8080/api )
+    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -u ${ROBOSERVER_MC_USERNAME}:${ROBOSERVER_MC_PASSWORD} http://localhost:8080/api/mc/cluster/clusters )
     if test ${HTTP_STATUS} = 200 ; then
         break
     fi
