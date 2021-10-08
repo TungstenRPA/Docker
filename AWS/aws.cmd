1318ee03ae16c1e8e8df5404c0ad0a7e20c1cd6d
David Wright <david.wright@kofax.com>
2021-10-08 20:12:00 +0200

    all steps to push RPA to amazon

--- AWS/aws.cmd
+++ AWS/aws.cmd
@@ -5,22 +5,26 @@ set AWS_Account=022336740566
 set registry=%AWS_Account%.dkr.ecr.%AWS_Region%.amazonaws.com
 docker context use default
 
-#docker compose build
+@REM Docker compose build
 
 docker tag managementconsole:%RPA_VERSION% %registry%/managementconsole:latest
+docker tag managementconsole:%RPA_VERSION% %registry%/managementconsole:%RPA_VERSION%
 docker tag roboserver:%RPA_VERSION% %registry%/roboserver:latest
+docker tag roboserver:%RPA_VERSION% %registry%/roboserver:%RPA_VERSION%
 
+@REM Docker logs into AWS
 aws ecr get-login-password --region %AWS_Region% | docker login --username AWS --password-stdin %registry%
 
+@REM Upload Docker images to Amazon Elastic Container Repository
 docker push %registry%/managementconsole:latest
 docker push %registry%/roboserver:latest
 
 docker context use aws
 
-docker compose convert
-# Deploy Kofax RPA to AWS and start it
-docker compose up 
-#docker compose down
-#docker compose logs
-#this gives the MC URL
+docker compose --env-file .env --env-file aws\aws.env convert
+@REM Deploy Kofax RPA to AWS and start it
+docker compose .env --env-file aws\aws.env up 
+@REM docker compose down
+@REM docker compose logs
+@REM this gives the MC URL
 docker compose ps
