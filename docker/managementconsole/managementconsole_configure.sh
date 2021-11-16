ca5c4af1af4f68fe7366ef6bb598516fb5e39b1d
David Wright <david.wright@kofax.com>
2021-11-16 15:23:55 +0100

    wait until restore finishes before creating users

--- docker/managementconsole/managementconsole_configure.sh
+++ docker/managementconsole/managementconsole_configure.sh
@@ -5,6 +5,8 @@ USERNAME=${MC_ADMIN_NAME}
 PASSWORD=${MC_ADMIN_PASSWORD}
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo SCRIPT_DIR=${SCRIPT_DIR}
+echo pwd
+pwd
 #Wait for Management Console to load
 MC_Wait()
 {
@@ -26,24 +28,33 @@ MC_Ping()
 
 MC_User_Exists()
 {
-    echo Check User ${USER_NAME} exists 
-    echo curl -s -u ${USERNAME}:${PASSWORD} "${MC}/api/mc/user/page"  grep "${USERNAME}" return 1
-         curl -s -u ${USERNAME}:${PASSWORD} "${MC}/api/mc/user/page" | grep '"${USERNAME}" || return' 1
+    echo Check User ${USERNAME} exists 
+    curl -s -u ${USERNAME}:${PASSWORD} "${MC}/api/mc/user/page" | grep '"${USERNAME}"' || return 1
+}
+
+MC_Group_Exists()
+{
+    echo Check User ${GROUPNAME} exists 
+    curl -s -u ${GROUPNAME}:${PASSWORD} "${MC}/api/mc/user/page" | grep '"${GROUPNAME}"' || return 1
 }
 
 # Call a POST/GET/PUT REST Webservice on Management Console
+
+#  this returns just the http code
+#   curl -i -s ...  2>/dev/nul | head -n 1 | cut -d$' ' -f2 | grep 200 || false
 MC_REST()
 {
     echo $@
     type=$1; path=$2; shift; shift ; data=$@ 
     test -f .json && cat .json
-    echo curl -i -S -u ${USERNAME}:${PASSWORD} ${data} -X ${type} "${MC}${path}"
-         curl -i -S -u ${USERNAME}:${PASSWORD} ${data} -X ${type} "${MC}${path}"
-    rc=$?
+    curl -i -u ${USERNAME}:${PASSWORD} ${data} -X ${type} "${MC}${path}" 2>&1 > curl.tmp
     rm -rf .json || true
-    if [ $rc != 0 ]; then
-        echo "${path} CURL failed with code: ${rc}"
-        return 1
+    cat curl.tmp >> curl.log
+    # check that the HTTP response was either 100 (continue) or 200 (ok)
+    cat curl.tmp | head -n 1 | cut -d$' ' -f2 | grep '100\|200' || false
+    if test $? != 0 ; then
+        echo CURL failed on $path
+        return false
     fi
     echo CURL succeeded
 }
@@ -128,20 +139,30 @@ while :
     done
 
 # MC_Change_Password ${USERNAME} admin ${PASSWORD}
-# The following are in the Backup package
-    # echo Add Groups
-    # MC_Add_Group Developers "build robots" ""
-    # MC_Add_Group Roboservers "run the robots" ""
-    # MC_Add_Group Synchronizers "upload and download robots to a Git Repository" ""
-    # MC_Add_Group KappletAdmins "create and edit Kapplets" ""
-    # MC_Add_Group KappletUsers "run Kapplets" ""
+# The Backup package contains
+#   * robots, groups, types
+#   * projects configures roles, groups and cluster.
+#   * cluster databases
+# NOTE * restore DELETES existing users
 
-#Create the roboserver user account so that the roboserver can actually get in and auto-register after the MC restore has completed
+# There is a timing problem.
+# We want to restore a backup and then add 3 users (dev, roboserver and sync), but the restore process returns success BEFORE it completes, and so the 3 users are added BEFORE the restore process completes,
+# and hence the users are deleted again when the restore process completes.
+# Work around.
+# 1. Add a user
+# 2. Do the restore
+# 3. Wait until restore has deleted this user and there is only 1 user - the admin user - by calling /api/mc/user/page and counting how many users there are
+# 4. Wait until restore has created 6 user groups
+# 5. Add the 3 users
 
+echo Adding user ${DEV_NAME}
+MC_Add_User "\"${DEV_NAME}\"" "\"${DEV_PASSWORD}\"" "\"${DEV_FULLNAME}\"" "\"${DEV_EMAIL}"\" '"RPA Administrators","Developers","RPA Administrators","KappletAdmins","KappletUsers"'
+# We just added a user the ensure that there are two users. the Restore will delete this user. We need to wait for Restore to completely finish before adding the users
 echo Zipping Backup package
 
-cd ${KAPOW_HOME}/backup
+pushd ${KAPOW_HOME}/backup
 zip -r backup.zip *
+popd
 echo Backup package zipped
 while :
     do
@@ -151,10 +172,31 @@ while :
     done
 echo Backup restored to MC
 
+# Wait until there is only 1 user - the admin user.
+while :
+    do
+        curl -s -u ${USERNAME}:${PASSWORD} "${MC}/api/mc/user/page" | grep -c '"count":1,"page"' && break
+        echo not 1 users
+        sleep 1
+    done
+echo all users deleted
+
+#Wait until all 6 groups are created
+while :
+    do
+        curl -s -u ${USERNAME}:${PASSWORD} "${MC}/api/mc/user/group/page" | grep '"count":6,"page"' && break
+        echo not 6 groups
+        sleep 1
+    done
+echo 6 groups created
+
+rm curl.log || true
+
 echo Adding Users
 echo Adding user ${DEV_NAME}
-MC_Add_User "\"${DEV_NAME}\"" "\"${DEV_PASSWORD}\"" "\"${DEV_FULLNAME}\"" "\"${DEV_EMAIL}"\" '"RPA Administrators","Developers","RPA Administrators","KappletAdmins","KappletUsers"' && break
+MC_Add_User "\"${DEV_NAME}\"" "\"${DEV_PASSWORD}\"" "\"${DEV_FULLNAME}\"" "\"${DEV_EMAIL}"\" '"RPA Administrators","Developers","RPA Administrators","KappletAdmins","KappletUsers"'
 
+#Create the roboserver user account so that the roboserver can actually get in and auto-register after the MC restore has completed
 echo Adding roboserver
 MC_Add_User "\"${ROBOSERVER_MC_USERNAME}\"" "\"${ROBOSERVER_MC_PASSWORD}\"" "'Roboserver'" "roboserver@rpa.com" '"Roboservers"'
 echo adding synch
