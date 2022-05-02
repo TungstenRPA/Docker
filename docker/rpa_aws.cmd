e19581bad8419ad7bd9efe99bc73d70d920be515
David Wright <david.wright@kofax.com>
2022-05-02 15:20:06 +0200

    more docker commands

--- docker/rpa_aws.cmd
+++ docker/rpa_aws.cmd
@@ -4,10 +4,14 @@ set region=eu-central-1
 set registry=%asw_account_id%.dkr.ecr.%region%.amazonaws.com
 
 
+# Docker Quickstart
+docker run  --publish 81:80 nginx:1.21.6-alpine
+
+
 # Download Kofax RPA Linux and go to the folder on the command line
-set RPA_version=11.2.0.5
+set RPA_version=11.2.0.7
 tar -xf d:\iso\rpa\KofaxRPA-%RPA_version%.tar.gz
-cd %RPA_version%
+cd *%RPA_version%*
 del *.rpm
 del *.deb
 git init
@@ -16,9 +20,13 @@ git fetch
 git checkout origin/master -ft
 
 
+
+
 # copy the docker-compose you require
 # copy docker\compose-examples\docker-compose-basic.yml docker-compose.yml
 docker compose --env-file docker\aws\aws.env -p rpa_%rpa_version% up
+docker tag managementconsole:%RPA_version%
+docker tag roboserver:%RPA_version%
 # login to Amazon Web Services
 aws ecr get-login-password --region %region% | docker login --username AWS --password-stdin %registry%
 docker tag managementconsole:latest %registry%/console:latest
